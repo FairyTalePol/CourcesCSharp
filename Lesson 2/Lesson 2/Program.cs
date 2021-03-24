949af59411bee078e32fedc74aff367af5db8d17
Егор Ус <egorusdnepr@gmail.com>
2021-03-24 14:58:18 +0200

    test commit

--- Lesson 2/Lesson 2/Program.cs
+++ Lesson 2/Lesson 2/Program.cs
@@ -6,7 +6,41 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //&& - проверяется до момента, пока будет гарантированный результат
+            //& - проверяется всё
+            //Аналогично для || |
+
+            int a = 3;
+            int b = 4;
+            bool c = a == b;
+            Console.WriteLine($"a = {a}; b = {b}");
+            Console.WriteLine($"a == b is {c}");
+            Console.WriteLine($"a != b is {a!=b}");
+            Console.WriteLine($"a > b is {a>b}");
+            Console.WriteLine($"a < b is {a<b}");
+            Console.WriteLine();
+
+            
+            Console.WriteLine($"true || false ---> {true||false}");
+            Console.WriteLine($"true && false ---> {true && false}");
+            Console.WriteLine($"true | false ---> {true | false}");
+            Console.WriteLine($"true & false ---> {true & false}");
+            Console.WriteLine();
+
+            int x = 1;
+            int y = 1;
+            int z = 1;
+            Console.WriteLine($"true | z++ > y || z++ <  ---> {true | z++ > y || z++ < y}");
+            Console.WriteLine(z);
+            Console.WriteLine($"true | x++>y || x++<y ---> {true || x++>y || x++<y}");
+            Console.WriteLine(x);
+            Console.WriteLine($"true | y++ > x | y++ < x ---> {true | y++ > x | y++ < x}");
+            Console.WriteLine(y);
+
+            Console.WriteLine($"true == !false ---> {true==!false}");
+            Console.ReadKey();
+
+
         }
     }
 }
