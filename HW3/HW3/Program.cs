adeac1fa29a20668d00722b02c66dfa75563a4c0
FairyTale <egorusdnepr@gmail.com>
2021-03-31 11:01:15 +0300

    Part of hw3

--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -6,27 +6,37 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            //Sum1to99();
+            //Task1();
 
-            /*for (int i=0;i<150;i++)
-            {
-                Console.WriteLine($"i={i}; prime={Prime(i)}");
-            }*/
+            //Task2();
 
-            //Console.WriteLine(Sqrt(44));
+            Console.WriteLine(Sqrt(16));
+            Console.WriteLine(Sqrt(17));
+            Console.WriteLine(Sqrt(18));
+            Console.WriteLine(Sqrt(19));
+            Console.WriteLine(Sqrt(20));
+            Console.WriteLine(Sqrt(21));
+            Console.WriteLine(Sqrt(22));
+            Console.WriteLine(Sqrt(23));
+            Console.WriteLine(Sqrt(24));
+            Console.WriteLine(Sqrt(25));
 
             //Console.WriteLine(Factorial(6));
 
-            //SummCifr(553);
+            //SummCifr(553); //Добавить ограничение на количество символов
 
             //Console.WriteLine(CountNumbers(123));
 
-            Console.WriteLine(Mirror(-1423523));
+            //Console.WriteLine(Mirror(-1423523));
+
+            //ProceedCalculations();
+
+            //Task8();
 
             Console.ReadKey();
         }
 
-        static void Sum1to99()
+        static void Task1()
         {
             int counter = 0;
             int sum = 0;
@@ -36,10 +46,28 @@ namespace ConsoleApp3
                 counter++;
                 //Console.WriteLine($"i={i}; counter={counter}; sum = {sum}");
             }
-            Console.WriteLine($"Sum = {sum}, number = {counter}");
+            Console.WriteLine($"Summ of odd number = {sum}, amount of odd numbers = {counter}");
         }
 
-        static bool Prime(double number)
+        public static void Task2()
+        {
+            Console.WriteLine("Enter positive number (>0):");
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double doubleNumber))
+            {
+                if (!int.TryParse(input, out int intNumber))
+                {
+                    Console.WriteLine("Only integer numbers can be prime");
+                    return;
+                }
+                Console.WriteLine($"Number {input} is {(Prime(intNumber)?"":"not")} prime");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+            }
+        }
+        static bool Prime(int number)
         {
             if (number < 2)
                 return false;
@@ -51,13 +79,19 @@ namespace ConsoleApp3
             return true;
         }
 
+        public static void Task3()
+        {
+
+        }
         static int Sqrt(int number)
         {
 
             for (int i = 0; i <= number / 2 + 1; i++)
             {
                 if (i * i > number)
-                    return i - 1;
+                {
+                    return Math.Abs(number - i * i) < Math.Abs(number - ((i - 1) * (i - 1)) )? i : i - 1;
+                }                
             }
             return 1;
         }
@@ -89,6 +123,12 @@ namespace ConsoleApp3
 
         public static int Mirror(int number) //Дописать условие для отрицательных чисел
         {
+            bool minus = false;
+            if (number<0)
+            {
+                number *= -1;
+                minus = true;
+            }    
             int mirror = 0;
             int count = CountNumbers(number);
             for (int i = 0; i < count; i++)
@@ -98,7 +138,7 @@ namespace ConsoleApp3
                 number = number / 10;
                 //Console.WriteLine($"Mirror = {mirror}; number = {number}");
             }
-            return mirror;
+            return !minus?mirror:mirror*-1;
         }
 
         public static int CountNumbers(int number)
@@ -119,7 +159,183 @@ namespace ConsoleApp3
             return number;
         }
 
+        public static void Task8()
+        {
+            do
+            {
+                Console.WriteLine("Please enter two numbers, one on each line");
+
+                string exceptionMsg = "Unsupported argument. Please use numbers";
+
+                if (!double.TryParse(Console.ReadLine(), out double number1))
+                {
+                    Console.WriteLine(exceptionMsg);
+                    continue;
+                }
+                if (!double.TryParse(Console.ReadLine(), out double number2))
+                {
+                    Console.WriteLine(exceptionMsg);
+                    continue;
+                }
+
+                if (number1 < 0)
+                    number1 *= -1;
+                if (number2 < 0)
+                    number2 *= -1;
+                Console.WriteLine(ContainSameNumbers(int.Parse(number1.ToString().Replace(",","")), int.Parse(number2.ToString().Replace(",", "")))+"\n");
 
 
+                /*Альтернативный способ избавления от запятой*/
+
+                //while (!int.TryParse(number1.ToString(), out int temp1))
+                //{
+                //    number1 *= 10;
+                //}
+                //while (!int.TryParse(number2.ToString(), out int temp2))
+                //{
+                //    number2 *= 10;
+                //}
+               
+                //Console.WriteLine(ContainSameNumbers(Convert.ToInt32(number1),Convert.ToInt32(number2)) + "\n");
+                
+                /*Конец альтернативного способа*/
+
+            } while (true);
+           
+        }
+
+        public static bool ContainSameNumbers(int a, int b)
+        {
+            int tempB = b;
+            while(a>0)
+            {
+                tempB = b;
+                while (tempB > 0)
+                {
+                    
+                    if (a % 10 == tempB% 10)
+                        return true;
+                    tempB /= 10;
+                }
+                a /= 10;
+               
+            }
+            return false;
+        }
+
+        public static void ProceedCalculations()
+        {
+            string exceptionMsg;
+            do
+            {
+                Console.WriteLine("Type calculation input (one line):");
+                exceptionMsg = "";
+                ParseCalculationInput(Console.ReadLine(),out exceptionMsg);
+                if (exceptionMsg != "")
+                    Console.WriteLine(exceptionMsg);
+            } while (true);
+        }
+        public static void ParseCalculationInput(string line, out string exceptionMsg)//Первые два встретившихся в строке аргумента и первая встретившаяся операция
+        {
+            while (line.Contains("."))
+                line = line.Replace(".", ",");
+            line = line.Trim();
+            line = RemoveMultiplySpace(line);
+
+            string[] operations = { "+", "-", "*", "/", "%" };
+
+            string onlyNumbers = line;
+            foreach (string o in operations)
+            {
+                onlyNumbers = onlyNumbers.Replace(o, " ");
+            }
+            onlyNumbers = onlyNumbers.Trim();
+            onlyNumbers = RemoveMultiplySpace(onlyNumbers);
+            //Console.WriteLine(onlyNumbers);//только аргументы, разделенные пробелом
+
+            string[] arguments = onlyNumbers.Split(' ');
+            if (arguments.Length < 2 || !double.TryParse(arguments[0], out double firstArgument) || !double.TryParse(arguments[1], out double secondArgument))
+            {
+                exceptionMsg="Invalid input, not enough arguments";
+                return;
+            }
+
+            string onlyOperations = line;
+            for (int i = 0; i < 10; i++)
+            {
+                while(onlyOperations.Contains(i.ToString()))
+                    onlyOperations = onlyOperations.Replace(i.ToString(), " ");
+            }
+            while (onlyOperations.Contains(","))
+                onlyOperations = onlyOperations.Replace(",", "");
+
+            onlyOperations = onlyOperations.Trim();
+            onlyOperations = RemoveMultiplySpace(onlyOperations);
+            //Console.WriteLine($"{onlyOperations}");
+            string[] presentOperations = onlyOperations.Split(' ');
+
+            if (presentOperations.Length<1 || presentOperations[0]=="")
+            {
+                exceptionMsg = "Invalid input, not enough operations";
+                return;
+            }
+            foreach(string o in presentOperations)
+            {
+                if (!Array.Exists(operations, element => element == o)){
+                    exceptionMsg = "Invalid input, unsupported operation";
+                    return;
+                }
+            }
+            if (presentOperations.Length!=arguments.Length-1)
+            {
+                exceptionMsg = "Invalid input, too many arguments or too many operations";
+                return;
+            }
+
+            //Console.WriteLine(onlyOperations);//только аргументы, разделенные пробелом
+            string exceptionCalculation;
+            double result = Calculate(double.Parse(arguments[0]), double.Parse(arguments[1]), presentOperations[0], out exceptionCalculation);
+
+            if (exceptionCalculation == "")
+                Console.WriteLine($"{arguments[0]} {presentOperations[0]} {arguments[1]} = {result}\n");
+            else
+                Console.WriteLine(exceptionCalculation);
+            
+            exceptionMsg = "";
+           
+        }
+        public static double Calculate(double a, double b, string operation, out string exceptionCalculation)
+        {
+            exceptionCalculation = "";
+            if ((operation == "/" || operation == "%")&&b==0)
+            {
+                exceptionCalculation = "Can't divide by zero";
+                return Int32.MinValue;
+            }
+                
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    return a / b;
+                case "%":
+                    return a % b;
+
+            }
+            Console.WriteLine("Unknown exception");
+            return Int32.MinValue;
+        }
+
+        public static string RemoveMultiplySpace(string line)
+        {
+            while (line.Contains("  "))
+                line = line.Replace("  ", " ");
+            return line;
+        }
     }
 }
